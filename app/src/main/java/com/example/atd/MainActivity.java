732fc15fc7f741efd556547046985e03ce61fee5
paxi07 <paxi070@gmail.com>
2019-03-24 09:02:09 +0000

    24/03/2018

--- app/src/main/java/com/example/atd/MainActivity.java
+++ app/src/main/java/com/example/atd/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.atd;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.example.atd.TabsDown.FragmentTabsDown_Add;
+import com.example.atd.TabsDown.FragmentTabsDown_Discover;
+import com.example.atd.TabsDown.FragmentTabsDown_Maps;
+import com.example.atd.TabsDown.FragmentTabsDown_Profile;
+import com.example.atd.TabsDown.FragmentTabsDown_Saved;
 
 public class MainActivity extends AppCompatActivity
 {
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+
+    FragmentTabsDown_Add fragmentTabsDown_add;
+    FragmentTabsDown_Discover fragmentTabsDown_discover;
+    FragmentTabsDown_Maps fragmentTabsDown_maps;
+    FragmentTabsDown_Profile fragmentTabsDown_profile;
+    FragmentTabsDown_Saved fragmentTabsDown_saved;
+
+    @Override
+    public void onBackPressed()
+    {
+        moveTaskToBack(true);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+        frameLayout = (FrameLayout) findViewById(R.id.framelayout_bottom);
+
+        fragmentTabsDown_add = new FragmentTabsDown_Add();
+        fragmentTabsDown_discover = new FragmentTabsDown_Discover();
+        fragmentTabsDown_maps = new FragmentTabsDown_Maps();
+        fragmentTabsDown_profile = new FragmentTabsDown_Profile();
+        fragmentTabsDown_saved = new FragmentTabsDown_Saved();
+
+        setFragment(fragmentTabsDown_discover);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item)
+            {
+
+                switch (item.getItemId())
+                {
+                    case R.id.tab_bottom_discover:
+                        setFragment(fragmentTabsDown_discover);
+                        return true;
+
+                    case R.id.tab_bottom_maps:
+                        setFragment(fragmentTabsDown_maps);
+                        return true;
+
+                    case R.id.tab_bottom_add:
+                        setFragment(fragmentTabsDown_add);
+                        return true;
+
+                    case R.id.tab_bottom_save:
+                        setFragment(fragmentTabsDown_saved);
+                        return true;
+
+                    case R.id.tab_bottom_profile:
+                        setFragment(fragmentTabsDown_profile);
+                    return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
+    }
+
+    public void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout_bottom, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/atd/TabsDown/FragmentTabsDown_Saved.java
@@ -0,0 +1,18 @@
+package com.example.atd.TabsDown;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.atd.R;
+
+public class FragmentTabsDown_Saved extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        return inflater.inflate(R.layout.fragment_tabsdown_saved, container, false);
+    }
+}
